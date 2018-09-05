23b34d774274c5850a16f7ee82131edda8cc030f
Hélio Luna <helioluna@outlook.com>
2018-09-05 01:53:45 -0300

    Cria converter para nome
    Cria converter onde obtem primeiro e o ultimo nome do usuário,
    concatenando e assim retornando-o.

---
+++ hemopet/hemopet.Core/Converters/NameConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace hemopet.Core.Converters
+{
+    public class NameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string fullName = value as string;
+            if (!String.IsNullOrEmpty(fullName))
+            {
+
+                List<string> names = fullName.Split(' ').ToList();
+                string firstName = names.First();
+                string lastName = names.Last();
+
+                if (firstName == lastName || String.IsNullOrEmpty(firstName) || String.IsNullOrEmpty(lastName))
+                    return String.IsNullOrEmpty(firstName) ? lastName : firstName;
+
+                return firstName + " " + lastName;
+            }
+
+            return fullName;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as string;
+        }
+    }
+}
