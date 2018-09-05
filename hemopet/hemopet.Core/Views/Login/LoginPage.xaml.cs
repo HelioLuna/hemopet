8b6c13d1f6d6eb3aab81c039f9deda75a4dba914
Hélio Luna <helioluna@outlook.com>
2018-09-04 23:40:58 -0300

    Adiciona Páginas
    Adiciona páginas de: Login e de Dados do Usuário

--- hemopet/hemopet.Core/Views/Login/LoginPage.xaml.cs
+++ hemopet/hemopet.Core/Views/Login/LoginPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using hemopet.Core.ViewModels.Login;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +8,36 @@ namespace hemopet.Core.Views.Login
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
     {
-		public LoginPage ()
+        private LoginViewModel vm;
+        private LoginViewModel ViewModel => vm ?? (vm = BindingContext as LoginViewModel);
+
+        public LoginPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            BindingContext = vm = new LoginViewModel(Navigation);
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            vm.OnCredencialValidated += delegate { OnCredencialValidatedAsync(); };
+            entrar.Clicked += delegate { OnEntrarClicked(); };
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        private void OnCredencialValidatedAsync()
+        {
+            Navigation.InsertPageBefore(new MainPage(), this);
+            Navigation.PopAsync();
+        }
+
+        private void OnEntrarClicked()
+        {
+            if (vm.ValidarCrendenciaisCommand.CanExecute(null))
+                vm.ValidarCrendenciaisCommand.Execute(null);
+        }
+    }
 }
---
+++ hemopet/hemopet.Core/Views/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using hemopet.Core.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace hemopet.Core.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : BottomBarPage
+    {
+        public MainPage()
+		{
+			InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            BarBackgroundColor = Color.White;
+
+            CustomNavigationPage DadosPage = new CustomNavigationPage(new MeusDados.DadosPage(Navigation), "Meus Dados");
+            CustomNavigationPage AnimaisPage = new CustomNavigationPage(new MeusDados.DadosPage(Navigation), "Meus Animais");
+            //CustomNavigationPage CronogramaPage = new CustomNavigationPage(new Cronograma.CronogramaPage(), "Cronograma");
+
+            Children.Insert(0, DadosPage);
+            Children.Insert(1, AnimaisPage);
+           // Children.Insert(2, CronogramaPage);
+        }
+	}
+}
