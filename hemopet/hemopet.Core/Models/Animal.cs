6a55614488ad11578905310447d25fa8778429de
Hélio Luna <jose.helio@saude.al.gov.br>
2018-09-10 17:13:00 -0300

    Cria Model de Animal

---
+++ hemopet/hemopet.Core/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hemopet.Core.Models
+{
+    public class Animal
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+        [JsonProperty("dat_nascimento")]
+        public DateTime DataNascimento { get; set; }
+        [JsonProperty("tipo")]
+        public TipoAnimalEnum Tipo { get; set; }
+    }
+}
