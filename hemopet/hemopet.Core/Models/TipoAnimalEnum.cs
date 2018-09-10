e9cd5b8febd849e0a51f6ebe5a8e432b6c8f4e63
Hélio Luna <jose.helio@saude.al.gov.br>
2018-09-10 17:13:39 -0300

    Cria Enum TipoAnimal
    Enum criado para facilitar a difenrenciação dentre os tipos de animais.

---
+++ hemopet/hemopet.Core/Models/TipoAnimalEnum.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace hemopet.Core.Models
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TipoAnimalEnum
+    {
+        [EnumMember(Value = "1"), Description("Cachorro")]
+        Cachorro,
+        [EnumMember(Value = "2"), Description("Gato")]
+        Gato
+    }
+}
