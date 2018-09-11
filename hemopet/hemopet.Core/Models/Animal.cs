73ae2e07f8e9b3d4ec8babe3759f1aaa8243af4e
Hélio Luna <jose.helio@saude.al.gov.br>
2018-09-11 17:15:55 -0300

    Cria Handlers para model animal

--- hemopet/hemopet.Core/Models/Animal.cs
+++ hemopet/hemopet.Core/Models/Animal.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using hemopet.Core.Helpers;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,12 +9,46 @@ namespace hemopet.Core.Models
     public class Animal
     {
         [JsonProperty("id")]
-        public string Id { get; set; }
+        public int Id { get; set; }
+        [JsonProperty("id_dono")]
+        public int IdDono { get; set; }
         [JsonProperty("nome")]
         public string Nome { get; set; }
         [JsonProperty("dat_nascimento")]
         public DateTime DataNascimento { get; set; }
         [JsonProperty("tipo")]
         public TipoAnimalEnum Tipo { get; set; }
+        [JsonIgnore]
+        public int Idade { get; set; }
+        [JsonIgnore]
+        public string DescricaoTipo { get; set; }
+        [JsonIgnore]
+        public string ImagemAnimal { get; set; }
+
+        public void HandleInfo()
+        {
+            HandleDescricaoTipo();
+            HandleIdadeAnimal();
+            HandleImagemAnimal();
+        }
+
+        private void HandleImagemAnimal()
+        {
+            if (Tipo == TipoAnimalEnum.Cachorro)
+                ImagemAnimal = "dog_icon.png";
+
+            if (Tipo == TipoAnimalEnum.Gato)
+                ImagemAnimal = "cat_icon.png";
+        }
+
+        private void HandleIdadeAnimal()
+        {
+            Idade = DateTime.Now.Year - DataNascimento.Year;
+        }
+
+        private void HandleDescricaoTipo()
+        {
+            DescricaoTipo = EnumHelper.GetDescription(Tipo);
+        }
     }
 }
